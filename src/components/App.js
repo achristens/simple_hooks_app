91c70e6ddf5eadf98cebe1c0cfec4eca5112cd2c
Abby Christens <abby.christens@cbc.ca>
2019-02-08 11:39:52 -0500

    Refactoring axios request function to its own file, reused code to make a new component in UserList

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import ResourceList from './ResourceList';
+import UserList from './UserList';
 
 const App = () => {
   const [resource, setResource] = useState("posts"); //the first variable 'resource' could be named anything, second "setResource"is a function call to update state
 
   return (
     <div>
+      <UserList />
       <div>
         <button onClick={() => setResource("posts")}>Posts</button>
         <button onClick={() => setResource("todos")}>To-Dos</button>

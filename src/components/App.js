f5f804b1279c9045564c5dce64e7d9e42262ae1f
Abby Christens <abby.christens@cbc.ca>
2019-02-08 11:19:07 -0500

    Installing axios, refactored class component to use hooks and render for new GET request

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ResourceList from './ResourceList';
 
 const App = () => {
   const [resource, setResource] = useState("posts"); //the first variable 'resource' could be named anything, second "setResource"is a function call to update state
@@ -7,9 +8,9 @@ const App = () => {
     <div>
       <div>
         <button onClick={() => setResource("posts")}>Posts</button>
-        <button onClick={() => setResource("to-dos")}>To-Dos</button>
+        <button onClick={() => setResource("todos")}>To-Dos</button>
       </div>
-      {resource}
+      <ResourceList resource={resource} />
     </div>
   );
 }
